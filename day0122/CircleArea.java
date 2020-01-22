91e66aa85b01175bf97f678e4f9459ff13b84b16
공필상 <gofeel8@hanmail.net>
2020-01-22 08:43:38 +0000

    Update day0122/CheckPoint.java, day0122/CircleArea.java, day0122/Computer.java files

---
+++ day0122/CircleArea.java
@@ -0,0 +1,9 @@
+package homework0122;
+
+public class CircleArea {
+
+	public static void main(String[] args) {
+		System.out.printf("반지름이 5Cm인 원의 넓이는 %.2f cm2입니다.",5*5*Math.PI);		
+	}
+
+}
---
+++ day0122/Computer.java
@@ -0,0 +1,16 @@
+package homework0122;
+
+import java.util.Scanner;
+
+public class Computer {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		System.out.println("곱="+a*b);
+		System.out.println("몫="+a/b);
+
+	}
+
+}
