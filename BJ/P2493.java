5412681b4cac9ec937a6486472a2c1e7484bbe26
gofeel8 <34854154+gofeel8@users.noreply.github.com>
2021-03-17 16:14:30 +0900

    Create P2493.java

---
+++ BJ/P2493.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+public class P2493 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader( System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		Stack<Integer> stack = new Stack<>();
+		Stack<Integer> height = new Stack<>();
+		
+		stack.add(-1);
+		height.add(-1);
+		
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i=0;i<N;i++) {
+			int num = Integer.parseInt(st.nextToken());
+			while(stack.peek()!=-1 && height.peek()<=num) {
+				stack.pop();
+				height.pop();
+			}
+			
+			if(stack.peek() == -1) {
+				bw.append("0 ");
+			}else { // 높은 벽을 만났을때
+				bw.append((stack.peek()+1)+" ");
+			}
+			stack.push(i);
+			height.push(num);
+			
+			
+		}
+		
+		bw.flush();
+		bw.close();
+		
+		
+	}
+
+}
